820958e6b6ff2b306f33f789ccbc9e29104eff93
scam627 <stiven.cardona627@gmail.com>
2019-12-02 19:16:29 -0500

    added save client controller

--- services/save_client.php
+++ services/save_client.php
@@ -1,3 +1,10 @@
 <?php
 	include("../config_db.php");
+	$name = isset($_POST["name"]) ?  $_POST["name"] : "";
+	$lastname = isset($_POST["lastname"]) ?  $_POST["lastname"] : "";
+	$phone = isset($_POST["phone"]) ?  $_POST["phone"] : "";
+	$email = isset($_POST["email"]) ?  $_POST["email"] : "";
+	
+	$result = $conn->query("INSERT INTO clients (name, lastname, email, phone) VALUES ('$name','$lastname','$email','$phone')");
+
 ?>
