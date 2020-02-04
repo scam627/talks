666ab520ab604ce8a2ce991de322d7411979e313
scam627 <stiven.cardona627@gmail.com>
2020-02-03 22:14:19 -0500

    nao input integrate

--- response.php
+++ response.php
@@ -22,14 +22,14 @@
 		href='https://fonts.googleapis.com/css?family=Noto+Serif%3A400%2C400i%2C700%2C700i&#038;ver=5.2.3'
 		type='text/css' media='all' />
 	<!-- jquery nao-input -->
-	<link rel="stylesheet" href="../src/css/style.css">
-	<link rel="stylesheet" href="../src/css/jquery-ui.min.css">
-	<link rel="stylesheet" href="../src/css/jquery-ui.structure.min.css">
-	<link rel="stylesheet" href="../src/css/jquery-ui.theme.min.css">
-	<link rel="stylesheet" href="../src/css/jquery.mCustomScrollbar.css">
-	<link rel="stylesheet" href="../src/css/jquery-pseudo-ripple.css">
-	<link rel="stylesheet" href="../src/css/jquery-nao-calendar.css">
-	<link rel="stylesheet" href="../src/css/jquery-nao-input.css">
+	<link rel="stylesheet" href="./src/css/style.css">
+	<link rel="stylesheet" href="./src/css/jquery-ui.min.css">
+	<link rel="stylesheet" href="./src/css/jquery-ui.structure.min.css">
+	<link rel="stylesheet" href="./src/css/jquery-ui.theme.min.css">
+	<link rel="stylesheet" href="./src/css/jquery.mCustomScrollbar.css">
+	<link rel="stylesheet" href="./src/css/jquery-pseudo-ripple.css">
+	<link rel="stylesheet" href="./src/css/jquery-nao-calendar.css">
+	<link rel="stylesheet" href="./src/css/jquery-nao-input.css">
 </head>
 
 <body id="cnt" class="bg-white">
@@ -65,14 +65,14 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
 		integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
 	</script>
-	<script src="../src/js/jquery-3.2.1.min.js"></script>
-	<script src="../src/js/jquery.min.js"></script>
-	<script src="../src/js/jquery-ui.min.js"></script>
-	<script src="../src/js/jquery-ui-touch-punch.min.js"></script>
-	<script src="../src/js/jquery.mCustomScrollbar.concat.min.js"></script>
-	<script src="../src/js/jquery-pseudo-ripple.js"></script>
-	<script src="../src/js/jquery-nao-calendar.js"></script>
-	<script src="../src/js/jquery-nao-input.js"></script>	
+	<script src="./src/js/jquery-3.2.1.min.js"></script>
+	<script src="./src/js/jquery.min.js"></script>
+	<script src="./src/js/jquery-ui.min.js"></script>
+	<script src="./src/js/jquery-ui-touch-punch.min.js"></script>
+	<script src="./src/js/jquery.mCustomScrollbar.concat.min.js"></script>
+	<script src="./src/js/jquery-pseudo-ripple.js"></script>
+	<script src="./src/js/jquery-nao-calendar.js"></script>
+	<script src="./src/js/jquery-nao-input.js"></script>	
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
 		integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
 	</script>
