af881c46db3e9e65512df5b7c41dce86d8542f0a
scam627 <stiven.cardona627@gmail.com>
2020-03-22 17:41:40 -0500

    load message

--- services/save_message.php
+++ services/save_message.php
@@ -1,6 +1,6 @@
 <?php
 	include("../config_db.php");
-	$message = isset($_POST["message"]) ? $_POST["message"] : "";
+	$message = !isset($_POST["message"]) ? $_POST["message"] : "";
 	$token = hash("sha256", $message);
 	$conn->query("INSERT INTO messages (token, message) VALUES ('$token','$message')");
 	echo json_encode(array("token" => $token, "message" => $message));
