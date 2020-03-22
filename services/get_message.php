23cfa7072ab27e611bae360675830b1692e23577
scam627 <stiven.cardona627@gmail.com>
2020-03-22 17:58:12 -0500

    load message

--- services/get_message.php
+++ services/get_message.php
@@ -2,6 +2,5 @@
 	include("../config_db.php");
 	$token = isset($_POST["token"]) ? $_POST["token"] : "";
 	$result = $conn->query("SELECT message FROM messages WHERE token = $token");
-	$message = $result->fetch_assoc()["message"];
-	echo json_encode(array("message" => $message));
+	echo json_encode($result->fetch_assoc());
 ?>
