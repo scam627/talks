fcf26e3b2ba176c698427994ce77bef261d3012c
scam627 <stiven.cardona627@gmail.com>
2020-03-22 19:22:24 -0500

    load message

--- services/get_message.php
+++ services/get_message.php
@@ -1,4 +1,5 @@
 <?php
+	header("Access-Control-Allow-Origin: https://scam627.github.io");
 	include("../config_db.php");
 	$token = isset($_POST["token"]) ? $_POST["token"] : "";
 	$result = $conn->query("SELECT message FROM messages WHERE token = '$token'");
