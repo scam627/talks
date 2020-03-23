120cb38648508ddf1675f4c9648d57745be13aa0
scam627 <stiven.cardona627@gmail.com>
2020-03-22 21:13:34 -0500

    load message

--- services/save_message.php
+++ services/save_message.php
@@ -2,7 +2,7 @@
 	header("Access-Control-Allow-Origin: https://scam627.github.io");
 	include("../config_db.php");
 	$message = isset($_POST["message"]) ? $_POST["message"] : "";
-	$token = hash("sha256", $message);
+	$token = uniqid();
 	$conn->query("INSERT INTO messages (token, message) VALUES ('$token','$message')");
 	echo json_encode(array("token" => $token, "message" => $message));
 ?>
