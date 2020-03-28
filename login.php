3aee3217c05272a8b38f891d54b02d23694459d3
scam627 <stiven.cardona627@gmail.com>
2020-03-27 19:14:54 -0500

    update charla to formulario

--- login.php
+++ login.php
@@ -1,12 +1,7 @@
 <?php include("./templates/header.php");?>
 <section>
 	<div class="row justify-content-center">
-		<div class="col-10 col-md-5 m-5 card p-5">
-			<div class="text-center">
-				<img src="./assets/images/cool.png" class="img-fluid rounded" style="height: 150px; width: 150px"
-					alt="...">
-			</div>
-			<br>
+		<div class="col-12 col-md-5 m-5 card p-5">
 			<form action="./services/validation.php" method="post">
 				<div class="form-row justify-content-center">
 					<div class="form-group col-md-6 col-12">
@@ -17,7 +12,7 @@
 				</div>
 				<div class="form-row justify-content-center">
 					<div class="form-group col-md-6 col-12">
-						<label for="inputPassword">Contraseña de la charla</label>
+						<label for="inputPassword">Contraseña del formulario</label>
 						<input type="password" class="form-control" id="inputPassword" placeholder="Pass" name="token" required>
 					</div>
 				</div>
